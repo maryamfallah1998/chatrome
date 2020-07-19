8966d69053ef356fd0eeb89ceb71e5a85e71afa3
v98fateme <v98fateme@gmail.com>
2020-07-19 18:03:10 +0430

    add massage length to header

--- Client.py
+++ Client.py
@@ -1,28 +1,32 @@
 import socket
 import select
-import errno
 
 
+HEADER_LENGTH = 10
 IP = "localhost"
 PORT = 9090
 
+
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((IP, PORT))
-
-
+client_socket.setblocking(False)
 
 while True:
     message = input('YOU > ')
 
-    if message:
-        message = message.encode('utf-8')
-        client_socket.send(message)
+    message = message.encode('utf-8')
+    message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+    client_socket.send(message_header + message)
 
-    
-        while True:
+  
+    while True:
 
-            message = client_socket.recv(1024).decode('utf-8')
-            print(' > ' + message)
+        message_header = client_socket.recv(HEADER_LENGTH)
+        message_length = int(message_header.decode('utf-8').strip())
+        message = client_socket.recv(message_length).decode('utf-8')
+
+        print(' > ' + message)
            
-
+  
     

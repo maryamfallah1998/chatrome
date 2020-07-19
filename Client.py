e1c9e063dfb1be5309326e1eb4d1c0672bbc9d4c
v98fateme <v98fateme@gmail.com>
2020-07-19 19:33:50 +0430

    IO

--- Client.py
+++ Client.py
@@ -1,16 +1,16 @@
 import socket
 import select
 
-
 HEADER_LENGTH = 10
+
 IP = "localhost"
 PORT = 9090
 
-
-
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((IP, PORT))
+
 client_socket.setblocking(False)
+
 
 while True:
     message = input('YOU > ')
@@ -19,14 +19,18 @@ while True:
     message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
     client_socket.send(message_header + message)
 
-  
-    while True:
+    try:
+        while True:
 
-        message_header = client_socket.recv(HEADER_LENGTH)
-        message_length = int(message_header.decode('utf-8').strip())
-        message = client_socket.recv(message_length).decode('utf-8')
+            message_header = client_socket.recv(HEADER_LENGTH)
+            message_length = int(message_header.decode('utf-8').strip())
+            message = client_socket.recv(message_length).decode('utf-8')
 
-        print(' > ' + message)
-           
-  
+            print('> > > ' + message)
+            
+    
+    except IOError as e:
+        continue
+
+    
     
